a0ed1c8e47ae43d3b3d858267720f98c9369ee75
eliya <eliyanakar@gmail.com>
2023-11-14 11:08:53 +0200

    adding initialization to redis

--- ingress/fileQa.py
+++ ingress/fileQa.py
@@ -1,6 +1,8 @@
 import json
 import os
 import re
+import pandas as pd
+import asyncio
 from langchain.docstore.document import Document
 from langchain.document_loaders import CSVLoader
 from langchain.text_splitter import CharacterTextSplitter, TokenTextSplitter, MarkdownTextSplitter, RecursiveCharacterTextSplitter, Language
@@ -15,8 +17,11 @@ from langchain.schema import (
     AIMessage,
     BaseMessage
 )
-
+from redisvl.index import SearchIndex
+from redisvl.query import VectorQuery
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+import sentence_transformers
+from redisvl.vectorize.text import HFTextVectorizer
 debug = True
 
 load_dotenv()
@@ -33,10 +38,13 @@ class csvQA:
         self.llm = None
         self.qa = None
         self.translator = None
+        self.index = None
 
     def download_embedding_module(self):
-        self.embedding =  SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        print("embdding cache folder:",self.embedding.cache_folder)
+        # self.embedding = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+        # print("embdding cache folder:",self.embedding.cache_folder)
+        self.embedding = HFTextVectorizer(model="sentence-transformers/all-MiniLM-L6-v2")
+        print("embdding loaded")
 
     def init_transalte(self):
         self.translator = Translate()
@@ -46,7 +54,7 @@ class csvQA:
         # create the open-source embedding function
         if self.embedding is None:
             self.download_embedding_module()
-        self.vectordb = Redis(index_name=redis_collection,embedding=self.embedding,redis_url=os.environ.get("REDIS_URL", "redis://localhost:6379"))
+        # self.vectordb = Redis(index_name=redis_collection,embedding=self.embedding,redis_url=os.environ.get("REDIS_URL", "redis://localhost:6379"))
         self.init_transalte()
 
     def init_llm(self) -> None:
@@ -64,19 +72,7 @@ class csvQA:
             verbose=True
         )
         
-        
-        # LlamaCpp(
-        #     model_path=os.environ.get("MODEL_PATH", "/Users/admin/Library/Application Support/nomic.ai/GPT4All/mistral-7b-instruct-v0.1.Q4_0.gguf"),
-        #     temperature=0.8,
-        #     max_tokens=1024,
-        #     top_p=1,
-        #     n_batch=100,
-        #     n_ctx=1024,
-        #     n_threads=32,
-        #     # callback_manager=callback_manager,
-        #     verbose=True
-        #     )
-        
+
     def get_chat(self):
         template = """
         you are army command and control summerize reports system, please answer the question
@@ -117,9 +113,9 @@ class csvQA:
 
         file_path = self.config.get("file_path",None)
         # vector_db_host = self.config.get("vector_db_host","localhost")
-        documents = CSVLoader(file_path=file_path,).load()
-            
-        print("Loaded {0} documents".format(len(documents)))
+        #documents = CSVLoader(file_path=file_path,).load()
+        df = pd.read_csv(file_path)
+        print("Loaded {0} documents".format(len(df)))
 
         # if not documents or all(isinstance(item, list) and not item for item in documents):
         #     print("No new documents found")
@@ -127,16 +123,39 @@ class csvQA:
              
         ##TODO: Validate if self.embedding is not None
         print("Creating vector db")
+        records = []
+        for _, record in df.iterrows():
+            i=1
+            new_record = record.copy(deep=True)
+            en_text = self.translator.translate_he_to_en(new_record["Message"])
+            en_vector = self.embedding.embed(en_text, as_buffer=True)
+            new_record["en_text"]=en_text
+            new_record["en_vector"] = en_vector
+            en_location = self.translator.translate_he_to_en(new_record["Location"])
+            en_loaction_vector = self.embedding.embed(en_location, as_buffer=True)
+            new_record["en_location"] = en_location
+            new_record["en_loaction_vector"] = bytes(en_loaction_vector)
+            records.append(new_record)
 
+        chunked_data = pd.DataFrame(records)
+        # Adding a running number column using range
+        chunked_data['RunningNumber'] = range(1, len(chunked_data) + 1)
+
+        self.index = SearchIndex.from_yaml("./data/hebrew_reports_schema.yaml")
+        self.index.connect("redis://localhost:6379")
+        self.index.create(overwrite=True)
+        self.index.load(chunked_data.to_dict(orient="records"))
+        i=1
         # map over all the docs and translate them
-        translated_docs = []
-        for doc in documents:
-            en_text = self.translator.translate_he_to_en(doc.page_content)
-            newDoc = Document(page_content=en_text,metadata=doc.metadata)
-            newDoc.metadata["he_text"] = doc.page_content
-            translated_docs.append(newDoc)
-            
-        self.vectordb.from_documents(documents=translated_docs,embedding=self.embedding,index_name=redis_collection)
+        # translated_docs = []
+        # for doc in documents:
+        #     en_text = self.translator.translate_he_to_en(doc.page_content)
+        #     newDoc = Document(page_content=en_text,metadata=doc.metadata)
+        #     newDoc.metadata["he_text"] = doc.page_content
+        #     # newDoc.metadata["content_heb_embed"] =
+        #     translated_docs.append(newDoc)
+        #
+        # self.vectordb.from_documents(documents=translated_docs,embedding=self.embedding,index_name=redis_collection)
 
     def answer_question(self,question:str) ->str:
         """
